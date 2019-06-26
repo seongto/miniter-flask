a1f06bc1f08fadb7006cd3699408d75883e6acbb
seongto <smalllife@naver.com>
2019-06-26 15:43:54 +0900

    인증까지 완료, 이제 unit test 시작

--- app.py
+++ app.py
@@ -1,30 +1,43 @@
-from flask import Flask, jsonify, request, current_app, g
+from flask import Flask, jsonify, request, current_app, Response, g
 from flask.json import JSONEncoder
 from sqlalchemy import create_engine, text
 import bcrypt
 import jwt
 from functools import wraps
+from datetime import datetime, timedelta
+from flask_cors import CORS
 
 
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         access_token = request.headers.get('Authorization')
-        if access_token is not None:
+        print("======================================")
+        print("decode해보자 : ", jwt.decode(access_token, current_app.config['JWT_SECRET_KEY'], 'HS256'))
+        print("======================================")
+        
+        if access_token is not None: 
             try:
+                print("======================================")
+                print("지금 엑세스토큰 상태 : ",access_token)
+                print("======================================")
+                print("지금 컨피그 쳌 : ",current_app.config)
+                
                 payload = jwt.decode(access_token, current_app.config['JWT_SECRET_KEY'], 'HS256')
             except jwt.InvalidTokenError :
                 payload = None
         
+            print("payload 상태 보고 : ", payload)
             if payload is None: return Response(status=401)
 
             user_id = payload['user_id']
             g.user_id = user_id
             g.user = get_user(user_id) if user_id else None
         else :
+            print("엑세스토큰 없대")
             return Response(status=401)
 
-        retrun f(*args, **kwargs)
+        return f(*args, **kwargs)
     return decorated_function
 
 
@@ -51,9 +64,9 @@ def get_user(user_id):
 
     return {
         "id": user['id'],
-        'name': row['name'],
-        'email': row['email'],
-        'profile': row['profile']
+        'name': user['name'],
+        'email': user['email'],
+        'profile': user['profile']
     } if user else None
 
 
@@ -135,6 +148,21 @@ def get_timeline(user_id):
     } for tweet in timeline ]
 
 
+def get_user_id_pw(email):
+    user_data = current_app.database.execute(text("""
+        SELECT
+            id,
+            hashed_password
+        FROM users
+        WHERE users.email = :email
+    """), { "email": email }
+    ).fetchone()
+
+    return {
+        "user_id": user_data['id'],
+        "hashed_password": user_data['hashed_password']
+    } if user_data else None
+
 
 def create_app(test_config = None):
     app = Flask(__name__)
@@ -161,30 +189,25 @@ def create_app(test_config = None):
         return "pong"
 
     
-    @app.route('/login', methods=['GET'])
+    @app.route('/login', methods=['POST'])
     def login():
         credential = request.json
         email = credential['email']
         password = credential['password']
+        user_data = get_user_id_pw(email)
 
-        row = database.execute(text("""
-            SELECT
-                id,
-                hashed_password
-            FROM users
-            WHERE email = :email
-        """), { 'email': email }).fetchone()
-
-        if row and bcrypt.checkpw(password.encode('UTF-8'), row['hashed_password'].encode('UTF-8')):
-            user_id = row['id']
+        if user_data and bcrypt.checkpw(password.encode('UTF-8'), user_data['hashed_password'].encode('UTF-8')):
+            user_id = user_data['user_id']
             payload = {
                 'user_id': user_id,
                 'exp': datetime.utcnow() + timedelta(seconds = 60 * 60 * 24)
             }
             token = jwt.encode(payload, app.config['JWT_SECRET_KEY'], 'HS256')
+            print("토큰간다! : ",token)
 
             return jsonify({
-                'access_token': token.decode('UTF-8')
+                'access_token': token.decode('UTF-8'),
+                'user_id': user_id
             })
         else :
             return '', 401
@@ -236,7 +259,7 @@ def create_app(test_config = None):
 
         return '', 200
 
-    @app.route('/timeline/<int:user_id>', methods=['POST'])
+    @app.route('/timeline/<int:user_id>', methods=['GET'])
     def timeline():
         return jsonify({
             'user_id' : user_id,
